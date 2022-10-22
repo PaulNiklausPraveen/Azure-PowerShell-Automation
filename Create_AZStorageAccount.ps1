9d3694036ae187c26de26508cfc54e90c26cbe14
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2022-10-22 22:11:36 +0530

    Create Create_AZStorageAccount.ps1

---
+++ Create_AZStorageAccount.ps1
@@ -0,0 +1,32 @@
+#Credential Parameters
+$AdminUserName = "username@domain.com"
+$AdminPassword = "TypeThePasswordHere"
+  
+#Create Variable for Pscredential object
+$SecurePassword = ConvertTo-SecureString $AdminPassword -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential -argumentlist $AdminUserName, $SecurePassword
+$Tenantid="TypeTenantid"
+   
+$AzureConnect=Connect-AzAccount –Credential $Credential -tenant " -Verbose
+ 
+If($AzureConnect)
+{
+Write-Host "Connected to Azure Cloud"
+}
+Else
+{
+  Write-Error 'Not connected to azure' -ErrorAction Stop
+}
+
+
+#Details required for Storage Account
+$AccountName = "psstaccount2022"
+$AccountKind="StorageV2"
+$AccountSKU="Standard_LRS"
+$ResourceGroupName="PowerShell-ResourceGroup"
+$Location = "eastus"
+
+$StorageAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $AccountName `
+-Location $Location -Kind $AccountKind -SkuName $AccountSKU
+
+$StorageAccount
