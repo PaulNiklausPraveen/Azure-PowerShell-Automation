348cfd118ad3b4e0c49f04b166b8700d772f864e
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2023-01-06 22:14:51 +0000

    Add files via upload

---
+++ Get_AZVMEncryptionStatus.ps1
@@ -0,0 +1,31 @@
+﻿<#
+This Script gets the encryption status of the virtual machine.
+It displays the encryption status of the operating system and data volumes. 
+#>
+
+#Set TLS to 1.2 version
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#Imports Azure Module
+Import-Module AZ -Force -Verbose
+
+#Mention the tenant id
+$TenantID = "WWWW-XXXX-YYYY-ZZZZ-1234"
+
+#Mention the Resourcegroup
+$ResourceGroup = "AzureResourceGroup-01"
+$VMName ="Azure_WindowsVM01"
+
+#Parameter for credentials
+$UserName = "username@domain.com"
+$Password = "P@55W0rd@23"
+  
+#Create a Pscredential object
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential -argumentlist $UserName, $SecurePassword
+   
+#Connect to Azure  
+Connect-AzAccount –Credential $Credential -Tenant $TenantID
+
+#Get Encryption status
+Get-AzVmDiskEncryptionStatus -ResourceGroupName $ResourceGroup -VMName $VMName
