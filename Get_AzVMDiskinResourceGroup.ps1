63b943beff65c32814588e63446460bef144665d
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2023-01-06 22:02:23 +0000

    Add files via upload

---
+++ Get_AzVMDiskinResourceGroup.ps1
@@ -0,0 +1,27 @@
+﻿#This gets the all Managed disks in a resource Group.
+
+#Set TLS to 1.2 version
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#Imports Azure Module
+Import-Module AZ -Force -Verbose
+
+#Mention the tenant id
+$TenantID = "WWWW-XXXX-YYYY-ZZZZ-1234"
+
+#Mention the Resourcegroup
+$ResourceGroup = "AzureResourceGroup-01"
+$VMName ="Azure_WindowsVM01"
+
+#Parameter for credentials
+$UserName = "username@domain.com"
+$Password = "P@55W0rd@23"
+  
+#Create a Pscredential object
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential -argumentlist $UserName, $SecurePassword
+   
+#Connect to Azure  
+Connect-AzAccount –Credential $Credential -Tenant $TenantID
+
+Get-AzDisk -ResourceGroupName $ResourceGroup  | Select Name, ResourceGroupNAme,OSType,DiskState,TimeCreated,Location | Format-Table -AutoSize
