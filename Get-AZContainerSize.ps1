80138e5e1638ecdc444d6e9b2109a4988dfd4119
Paul Niklaus Praveen <62203157+PaulNiklausPraveen@users.noreply.github.com>
2023-01-09 18:42:56 +0000

    Add files via upload

---
+++ Get-AZContainerSize.ps1
@@ -0,0 +1,31 @@
+﻿Function Get-AZContainerSize
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$ResourceGroupName,
+        [string]$StorageAccountName,
+        [string]$ContainerName
+        
+         
+    )
+
+$StorageAccountData=Get-AzStorageAccount -Name $StorageAccountName -ResourceGroupName $ResourceGroupName
+
+$ContainerData=Get-AzStorageContainer -Name $ContainerName -Context $StorageAccountData.Context
+
+$BlobsData=Get-AzStorageBlob -Name $ContainerName -Context $StorageAccountData.Context
+
+$Init=0
+
+foreach($Blob in $BlobsData)
+{
+    $Init+=$Blob.Length
+}
+
+$Size= ($Init/1024/1024)
+
+"`nTotal Size of files in the Container $ContainerName " + [math]::Round($Size,2) + " MB"
+}
+
+ 
