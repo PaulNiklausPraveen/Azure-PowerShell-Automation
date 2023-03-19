3456f7c78a1bb5402e3b14178dcac6f9d2d1702c
Paul Niklaus Praveen <paulnikpraveen@gmail.com>
2023-03-19 16:28:27 +0530

    Create a Network Security Group

---
+++ Create-AZNetworkSecurityGroup.ps1
@@ -0,0 +1,29 @@
+
+$NetworkSecurityGroupName="AZ-SecurityGroup1"
+$ResourceGroupName ="ResourceGroup1"
+$Location="North Europe"
+
+
+$SecurityRule1=New-AzNetworkSecurityRuleConfig -Name "Allow-RDP" -Description "Allow RDP Traffice Flow" `
+-Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix * -SourcePortRange * `
+-DestinationAddressPrefix * -DestinationPortRange 3389
+
+$SecurityRule2=New-AzNetworkSecurityRuleConfig -Name "Allow-HTTPS" -Description "Allow HTTPS Traffic Flow" `
+-Access Allow -Protocol Tcp -Direction Inbound -Priority 101 -SourceAddressPrefix * -SourcePortRange * `
+-DestinationAddressPrefix * -DestinationPortRange 443
+
+$NetworkSecurityGroup=New-AzNetworkSecurityGroup -Name $NetworkSecurityGroupName `
+-ResourceGroupName $ResourceGroupName -Location $Location `
+-SecurityRules $SecurityRule1,$SecurityRule2
+
+ 
+$VirtualNetworkName="lb1-vnet"
+$SubnetName="subnet1"
+$SubnetAddressSpace="10.0.0.0/24"
+
+$VirtualNetwork=Get-AzVirtualNetwork -Name $VirtualNetworkName -ResourceGroupName $ResourceGroupName
+
+Set-AzVirtualNetworkSubnetConfig -Name $SubnetName -VirtualNetwork $VirtualNetwork -NetworkSecurityGroup $NetworkSecurityGroup `
+-AddressPrefix $SubnetAddressSpace
+
+$VirtualNetwork | Set-AzVirtualNetwork
