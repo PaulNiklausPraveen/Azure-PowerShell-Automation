0a68e93f44e8d958b73fe2140667b032cc93f7cc
Paul Niklaus Praveen <paulnikpraveen@gmail.com>
2023-03-19 21:43:20 +0530

    Create Lock for Azure Resources-added validate set

--- Create-AZLock.ps1
+++ Create-AZLock.ps1
@@ -1,6 +1,12 @@
 Function Lock-AZResource{
 
-param([String] $ResourceName)
+param(
+[Parameter(Mandatory)]
+[String] $ResourceName,
+[Parameter(Mandatory)]
+[ValidateSet("ReadOnly","CanNotDelete")]
+[string] $LockLevel
+)
 
 
 Function Get-AZResourceGroup
@@ -12,7 +18,7 @@ Function Get-AZResourceGroup
 
 $ResourceType=(Get-AzResource -Name $ResourceName).Type
 
-New-AzResourceLock -LockLevel ReadOnly -LockName "ReadOnlyLock-$($ResourceName)" -ResourceName $ResourceName -ResourceType $ResourceType `
+New-AzResourceLock -LockLevel $LockLevel -LockName "ReadOnlyLock-$($ResourceName)" -ResourceName $ResourceName -ResourceType $ResourceType `
 -ResourceGroupName (Get-AZResourceGroup $ResourceName) -Force 
 
 
