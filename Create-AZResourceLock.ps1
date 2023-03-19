7c2feaac27a74afa8f21df932fece1b0ff991df2
Paul Niklaus Praveen <paulnikpraveen@gmail.com>
2023-03-19 21:44:45 +0530

    Create lock for Azure Resources

---
+++ Create-AZResourceLock.ps1
@@ -0,0 +1,29 @@
+#This PS Script will lock the Azure resources
+
+Function Lock-AZResource{
+
+param(
+[Parameter(Mandatory)]
+[String] $ResourceName,
+[Parameter(Mandatory)]
+[ValidateSet("ReadOnly","CanNotDelete")]
+[string] $LockLevel
+)
+
+
+Function Get-AZResourceGroup
+{
+    Param([String] $ResourceName)
+    $Resource=Get-AzResource -Name $ResourceName
+    Return $Resource.ResourceGroupName
+}
+
+$ResourceType=(Get-AzResource -Name $ResourceName).Type
+
+New-AzResourceLock -LockLevel $LockLevel -LockName "ReadOnlyLock-$($ResourceName)" -ResourceName $ResourceName -ResourceType $ResourceType `
+-ResourceGroupName (Get-AZResourceGroup $ResourceName) -Force 
+
+
+
+} 
+ 
