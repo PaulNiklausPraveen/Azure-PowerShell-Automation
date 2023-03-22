5675af6f7abd78a89d7e18a18bdc9a1d60897f8c
Paul Niklaus Praveen <paulnikpraveen@gmail.com>
2023-03-22 12:40:44 +0530

    Move resources to another RG in Azure

---
+++ Move-AZResource.ps1
@@ -0,0 +1,45 @@
+<#
+This PowerShell Script will Move the Resources to another Resource Group
+Requires -Version 7.0 
+#>
+
+#Connect-AzAccount
+
+$ResourceName="vm1"
+$DestinationResourceGroupName="rg2"
+
+#Define Functions
+Function Get-ResourceGroupID {
+    param([String] $ResourceGroupName)
+    $ResourceGroupID=(Get-AzResourceGroup -Name $ResourceGroupName).ResourceId
+    Return $ResourceGroupID
+}
+Function Get-ResourceGroupName {
+    Param ([String] $ResourceName)
+    $ResourceGroupName=(Get-AzResource -Name $ResourceName).ResourceGroupName
+    Return $ResourceGroupName    
+}
+Function Get-ResourceId {
+    Param ([String] $ResourceName)
+    $ResourceID=(Get-AzResource -Name $ResourceName).ResourceId
+    Return $ResourceID
+}
+$ResourceId=(Get-ResourceId $ResourceName)
+$SourceResourceGroupName=(Get-ResourceGroupName $ResourceName)
+$SourceResourceGroupId=(Get-ResourceGroupID $SourceResourceGroupName)
+$DestinationResourceGroupId=(Get-ResourceGroupID $DestinationResourceGroupName)
+
+Try {
+#validate if resource can move to other resourcegroup
+Invoke-AzResourceAction -Action validateMoveResources -ResourceId $SourceResourceGroupId -Parameters `
+@{resources=@($ResourceId);targetResourceGroup= $DestinationResourceGroupId} -Force -ErrorAction Stop
+
+Move-AzResource -DestinationResourceGroupName $DestinationResourceGroupName -ResourceId $ResourceId -Force
+}
+Catch {
+$_.Exception.Message
+}
+
+#Validate if resource has been moved or not
+(Get-ResourceGroupName $ResourceName) -Eq $DestinationResourceGroupName  ? "$ResourceName has been moved to $DestinationResourceGroupName" :  `
+  "Failed to move the resource $ResourceName to $DestinationResourceGroupName Group"
