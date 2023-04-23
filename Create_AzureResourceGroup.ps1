aa61dd1f6b515abf49871f01381a03cb9da2c764
Paul Niklaus Praveen <paulnikpraveen@gmail.com>
2023-04-23 19:57:39 +0530

    Update Create_AzureResourceGroup.ps1

--- Create_AzureResourceGroup.ps1
+++ Create_AzureResourceGroup.ps1
@@ -9,9 +9,9 @@ Disable-AzContextAutosave
 #Replace the data with your custom names.
 #Use "Get-AZLocation | FT " to get location name if required
 
-$RGName="ResourceGroupName"
-$LocationName= "Location"
-$Tags= @{Key="Value"}
+$RGName="AZ-RG-PS"
+$LocationName= "North Europe"
+$Tags= @{ENV="Stage"}
 
 #Create a new resource group in azure with above mentioned details.
 New-AzResourceGroup -Name $RGName -Location $LocationName -Tag $Tags -Verbose
